56e6612f3a8d7e1f018238ecea976a56303354bd
Alberto Barradas <abcsds@gmail.com>
2015-05-28 16:30:06 -0500

    First views in Dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+from django.core import serializers
 
-# Create your views here.
+
+def dashboard(request):
+    tweets = []
+    for tweet in SenTweet.objects.all():
+        if tweet.lang == 'en':
+            tweets.append(tweet)
+        else:
+            # delete object
+            pass
+    tweets = serializers.serialize("json", tweets, fields=('user_screen_name','text'))
+    return HttpResponse(tweets)
--- tw/urls.py
+++ tw/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^stream/', include('twitter_stream.urls', namespace="twitter_stream")),
+    url(r'^$', include('dashboard.urls')),
 ]
