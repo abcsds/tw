4adfc336c8a84856bbe37a1eb3e0141fd8444536
Alberto Barradas <abcsds@gmail.com>
2015-05-28 01:10:09 -0500

    Updated settings for stream url

--- tw/settings.py
+++ tw/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'twitter_stream',
+    'django.contrib.humanize',
+    'bootstrap3',
+    'jsonview',
 )
 
 MIDDLEWARE_CLASSES = (
