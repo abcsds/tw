c4309f210523f2f94887b3e8433dc6dcd02c196b
Alberto Barradas <abcsds@gmail.com>
2015-05-28 13:44:28 -0500

    Configured Admin Site

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,4 +1,42 @@
 from django.contrib import admin
 from .models import SenTweet, WordScore
-admin.site.register(SenTweet)
-admin.site.register(WordScore)
+
+class SenTweetAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Tweet',            {'fields': ['user_screen_name', 'text', 'lang']}),
+        ('Date information', {'fields': [   'tweet_id',
+                                            'truncated',
+                                            'user_id',
+                                            'user_name',
+                                            'user_verified',
+                                            'created_at',
+                                            'user_utc_offset',
+                                            'user_time_zone',
+                                            'filter_level',
+                                            'latitude',
+                                            'longitude',
+                                            'user_geo_enabled',
+                                            'user_location',
+                                            'favorite_count',
+                                            'retweet_count',
+                                            'user_followers_count',
+                                            'user_friends_count',
+                                            'in_reply_to_status_id',
+                                            'retweeted_status_id',
+                                            'sentiment'
+                                        ],
+
+                                        'classes': ['collapse']
+                            }),
+    ]
+    list_display = ('user_screen_name', 'created_at', 'lang')
+    list_filter = ['lang']
+    search_fields = ['user_screen_name']
+class WordScoreAdmin(admin.ModelAdmin):
+    fields = ['word', 'score']
+
+    list_display = ('word', 'score')
+    search_fields = ['word']
+
+admin.site.register(SenTweet, SenTweetAdmin)
+admin.site.register(WordScore, WordScoreAdmin)
--- dashboard/migrations/0001.sql
+++ dashboard/migrations/0001.sql
@@ -1,6 +1,34 @@
 BEGIN;
-CREATE TABLE `dashboard_sentweet` (`id` bigint UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY, `tweet_id` bigint NOT NULL, `text` varchar(250) NOT NULL, `truncated` bool NOT NULL, `lang` varchar(9) NULL, `user_id` bigint NOT NULL, `user_screen_name` varchar(50) NOT NULL, `user_name` varchar(150) NOT NULL, `user_verified` bool NOT NULL, `created_at` datetime(6) NOT NULL, `user_utc_offset` integer NULL, `user_time_zone` varchar(150) NULL, `filter_level` varchar(6) NULL, `latitude` double precision NULL, `longitude` double precision NULL, `user_geo_enabled` bool NOT NULL, `user_location` varchar(150) NULL, `favorite_count` integer UNSIGNED NULL, `retweet_count` integer UNSIGNED NULL, `user_followers_count` integer UNSIGNED NULL, `user_friends_count` integer UNSIGNED NULL, `in_reply_to_status_id` bigint NULL, `retweeted_status_id` bigint NULL, `sentiment` integer NOT NULL);
-CREATE TABLE `dashboard_wordscore` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `word` varchar(200) NOT NULL, `score` integer NOT NULL);
+CREATE TABLE `dashboard_sentweet` (
+    `id` bigint UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `tweet_id` bigint NOT NULL,
+    `text` varchar(250) NOT NULL,
+    `truncated` bool NOT NULL,
+    `lang` varchar(9) NULL,
+    `user_id` bigint NOT NULL,
+    `user_screen_name` varchar(50) NOT NULL,
+    `user_name` varchar(150) NOT NULL,
+    `user_verified` bool NOT NULL,
+    `created_at` datetime(6) NOT NULL,
+    `user_utc_offset` integer NULL,
+    `user_time_zone` varchar(150) NULL,
+    `filter_level` varchar(6) NULL,
+    `latitude` double precision NULL,
+    `longitude` double precision NULL,
+    `user_geo_enabled` bool NOT NULL,
+    `user_location` varchar(150) NULL,
+    `favorite_count` integer UNSIGNED NULL,
+    `retweet_count` integer UNSIGNED NULL,
+    `user_followers_count` integer UNSIGNED NULL,
+    `user_friends_count` integer UNSIGNED NULL,
+    `in_reply_to_status_id` bigint NULL,
+    `retweeted_status_id` bigint NULL,
+    `sentiment` integer NOT NULL
+);
+CREATE TABLE `dashboard_wordscore` (
+    `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `word` varchar(200) NOT NULL,
+    `score` integer NOT NULL);
 CREATE INDEX `dashboard_sentweet_fde81f11` ON `dashboard_sentweet` (`created_at`);
 
 COMMIT;
