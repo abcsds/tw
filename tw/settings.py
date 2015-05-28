6054b0cf70e447fdd6f274fc7d826fe6f8bdc3c6
Alberto Barradas <abcsds@gmail.com>
2015-05-28 01:03:53 -0500

    Updated Database Name

--- tw/settings.py
+++ tw/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'tw.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'local',
+        'NAME': 'tw',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
