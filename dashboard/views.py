6820dec7c2007150f81d7043f5ec2cc8044b33c7
Alberto Barradas <abcsds@gmail.com>
2015-05-28 19:12:32 -0500

    Url, view and template for dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,16 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# from django.template import RequestContext, loader
+# from django.core import serializers
 from .models import *
-from django.core import serializers
 
+# API for json
+# def dashboard(request):
+#     tweets = []
+#     for tweet in SenTweet.objects.all():
+#         if tweet.lang == 'en':
+#             tweets.append(tweet)
+#         else:
+#             # delete object
+#             pass
+#     tweets = serializers.serialize("json", tweets, fields=('user_screen_name','text'))
+#     return HttpResponse(tweets)
 
 def dashboard(request):
     tweets = []
-    for tweet in SenTweet.objects.all():
-        if tweet.lang == 'en':
-            tweets.append(tweet)
-        else:
-            # delete object
-            pass
-    tweets = serializers.serialize("json", tweets, fields=('user_screen_name','text'))
-    return HttpResponse(tweets)
+    try:
+        for tweet in SenTweet.objects.all():
+            if tweet.lang == 'en':
+                tweets.append(tweet)
+    except SenTweet.DoesNotExist:
+        raise Http404("No tweets found: run stream.")
+    context = {'tweets': tweets}
+    return render(request, 'dashboard/dashboard.html', context)
+
+# # Update all scores
+# def updateScores(request):
+#     for word in WordScore.objects.all():
+#         #update word score
+#         pass
+#     pass
+#
+# # Upload a wordlist.csv
+# def uploadWordlist(request):
+#
+#     pass
