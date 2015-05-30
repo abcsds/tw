633b7d81244b8acb46dd9c12f0b9deefe3ed7a6d
Alberto Barradas <abcsds@gmail.com>
2015-05-29 21:46:19 -0500

    Added SQL

---
+++ dashboard/sql/SenTweet.sql
@@ -0,0 +1,15 @@
+CREATE TRIGGER input
+AFTER INSERT
+   ON dashboard_sentweet FOR EACH ROW
+
+BEGIN
+
+   -- variable declarations
+   DECLARE score int;
+
+   -- trigger code
+   -- insert
+
+
+
+END;
